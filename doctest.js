aab80894135d649b7027f1fe595260e2b17efae5
nishant goyal <nishantgoyal880@gmail.com>
2018-02-08 02:15:50 +0530

    initial results achieved

--- doctest.js
+++ doctest.js
@@ -25,26 +25,21 @@ init(countT);
                 }else{
                       //counting word
                       count[0]=wordcount(text);
-                      console.log(count[0]);
                       //counting nouns
                       wordpos.getNouns(text, function(result){
                       count[1]=result.length;
-                      console.log(count[1]);
                       });
                       //counting adjectives
                       wordpos.getAdjectives(text, function(result){
                       count[2]=result.length;
-                      console.log(count[2]);
                       });
                       //counting verbs
                       wordpos.getVerbs(text, function(result){
                       count[3]=result.length;
-                      console.log(count[3]);
                       });
                       //counting adverbs
                       wordpos.getAdverbs(text, function(result){
                       count[4]=result.length;
-                      console.log(count[4]);
                       });
                     }
                     setTimeout(function(){
@@ -58,26 +53,32 @@ init(countT);
         	var score=200;
         	var wordPerc= parseInt((countA[0]-countT[0])/countA[0]*100);
         	var wordPerc = (wordPerc < 0) ? wordPerc * -1 : wordPerc;
-        	console.log(wordPerc);
+        	
         	var nounPerc=parseInt((countA[1]-countT[1])/countA[1]*100);
         	var nounPerc = (nounPerc < 0) ? nounPerc * -1 : nounPerc;
-        	console.log(nounPerc);
+        	
         	var adjPerc=parseInt((countA[2]-countT[2])/countA[2]*100);
         	var adjPerc = (adjPerc < 0) ? adjPerc * -1 : adjPerc;
-        	console.log(adjPerc);
+        	
         	var verbPerc=parseInt((countA[3]-countT[3])/countA[3]*100);
         	var verbPerc = (verbPerc < 0) ? verbPerc * -1 : verbPerc;
-        	console.log(verbPerc);
+        	
         	var adPerc=parseInt((countA[4]-countT[4])/countA[4]*100);
         	var adPerc = (adPerc < 0) ? adPerc * -1 : adPerc;
-        	console.log(adPerc);
+        	
+        	var average=(wordPerc+nounPerc+adPerc+adjPerc+verbPerc)/5;
 
     		if(countT[0]>1500 && countT[0]<5000){
+    			score=score-wordPerc;
+    			score=score-nounPerc;
 
 
-
+    		console.log('The overall score of target document with respect to admin document is '+score+" out of 200");
+    		console.log("");
     		}else{
     		console.log("Document word count is less therefore rejected");
   			}
+  			console.log("The overall percentage of document in terms of literature is "+average+" %");
+  			console.log("");
   			console.log("-------------Analysis completed-----------");
         });
