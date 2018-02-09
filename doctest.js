fd6c4593679347f9934a9874ed3acf10d86d0413
sapient <nishantgoyal880@gmail.com>
2018-02-09 21:52:03 +0530

    objects created

--- doctest.js
+++ doctest.js
@@ -1,84 +1,100 @@
 var textract = require('textract');
-//var natural = require('natural');
+var natural = require('natural');
+var stringSimilarity = require('string-similarity');
 var wordcount = require('wordcount');
 var WordPOS = require('wordpos');
-wordpos = new WordPOS();
-var countT=new Array(6);
-var countA=new Array(6);
-var i;
+var fs=require('fs');
+var path = require('path');
+var wordpos = new WordPOS();
+var admin='html.docx';
+var target='bootstrap.docx';
+var dataA;
+var dataT;
 
-function init(count){
-  for(i=0;i<6;i++){
-    count[i]=0;
-  }
+
+var adminData={
+  extA:path.extname(admin),
+  wordcount:0,
+  nounsCount:0,
+  adjCount:0,
+  verbCount:0,
+  nounPercA:0,
+  adjPercA:0,
+  verbPercA:0,
 }
-//initializing arrays
-init(countA);
-init(countT);
+var targetData={
+  extT:path.extname(target),
+  wordcount:0,
+  nounsCount:0,
+  adjCount:0,
+  verbCount:0,
+  nounPercT:0,
+  adjPercT:0,
+  verbPercT:0,
+}
+var compData={
+  stringMatch:0,
+}
 
-           var docBreak = function(file,count){
-             return new Promise(function(resolve,reject){
-              //reading file
-              textract.fromFileWithPath(file,{preserveLineBreaks:true},function(error,text){
-                if (error){
-                  console.log(error);
-                }else{
-                      //counting word
-                      count[0]=wordcount(text);
-                      //counting nouns
-                      wordpos.getNouns(text, function(result){
-                      count[1]=result.length;
-                      });
-                      //counting adjectives
-                      wordpos.getAdjectives(text, function(result){
-                      count[2]=result.length;
-                      });
-                      //counting verbs
-                      wordpos.getVerbs(text, function(result){
-                      count[3]=result.length;
-                      });
-                      //counting adverbs
-                      wordpos.getAdverbs(text, function(result){
-                      count[4]=result.length;
-                      });
-                    }
-                    setTimeout(function(){
-    		      		resolve("Success!"); 
-                 		}, 250);
-                  });                
-              });
-            }
-   
-        Promise.all([docBreak('html.docx',countA),docBreak('bootstrap.docx',countT)]).then(function(){
-        	var score=200;
-        	var wordPerc= parseInt((countA[0]-countT[0])/countA[0]*100);
-        	var wordPerc = (wordPerc < 0) ? wordPerc * -1 : wordPerc;
-        	
-        	var nounPerc=parseInt((countA[1]-countT[1])/countA[1]*100);
-        	var nounPerc = (nounPerc < 0) ? nounPerc * -1 : nounPerc;
-        	
-        	var adjPerc=parseInt((countA[2]-countT[2])/countA[2]*100);
-        	var adjPerc = (adjPerc < 0) ? adjPerc * -1 : adjPerc;
-        	
-        	var verbPerc=parseInt((countA[3]-countT[3])/countA[3]*100);
-        	var verbPerc = (verbPerc < 0) ? verbPerc * -1 : verbPerc;
-        	
-        	var adPerc=parseInt((countA[4]-countT[4])/countA[4]*100);
-        	var adPerc = (adPerc < 0) ? adPerc * -1 : adPerc;
-        	
-        	var average=(wordPerc+nounPerc+adPerc+adjPerc+verbPerc)/5;
+var docBreak = function(file,obj,data){
+  return new Promise((resolve,reject)=>{
+      //reading file
+      textract.fromFileWithPath(file,{preserveLineBreaks:true},function(error,text){
+        if (error){
+          console.log(error);
+        }else{
+          //obtaining whole text
+          if(data=="A")
+            dataA=text;
+          if(data=='T')
+            dataT=text;
+          //counting word
+          obj.wordcount=wordcount(text);
+          //counting nouns
+          wordpos.getNouns(text, function(result){
+            obj.nounsCount=result.length;
+          });
+          //counting adjectives
+          wordpos.getAdjectives(text, function(result){
+            obj.adjCount=result.length;
+          });
+          //counting verbs
+          wordpos.getVerbs(text, function(result){
+            obj.verbCount=result.length;
+            resolve("success");
+          });
+        }
+      });
+    });
+  }
 
-    		if(countT[0]>1500 && countT[0]<5000){
-    			score=score-wordPerc;
-    			score=score-nounPerc;
+let promises=[];
+let p1=docBreak(admin,adminData,"A");
+promises.push(p1);
+let p2=docBreak(target,targetData,"T");
+promises.push(p2);
+
+Promise.all(promises).then(function(results){
+  var score=200;
+
+  //calculating %age of nouns
+   adminData.nounPercA=parseInt(adminData.nounsCount/(adminData.wordcount)*100);
+   targetData.nounPercT=parseInt(targetData.nounsCount/(targetData.wordcount)*100);
+
+  //calculating %age of adjectives
+   adminData.adjPercA=parseInt(adminData.adjCount/(adminData.wordcount)*100);
+   targetData.adjPercT=parseInt(targetData.adjCount/(targetData.wordcount)*100);
+
+  //calculating %age of verbs
+   adminData.verbPercA=parseInt(adminData.verbCount/(adminData.wordcount)*100);
+   targetData.verbPercT=parseInt(targetData.verbCount/(targetData.wordcount)*100);
+
+  //calculating string matching
+   compData.stringMatch=(stringSimilarity.compareTwoStrings(dataA,dataT))*100;
 
 
-    		console.log('The overall score of target document with respect to admin document is '+score+" out of 200");
-    		console.log("");
-    		}else{
-    		console.log("Document word count is less therefore rejected");
-  			}
-  			console.log("The overall percentage of document in terms of literature is "+average+" %");
-  			console.log("");
-  			console.log("-------------Analysis completed-----------");
-        });
+   console.log(adminData);
+   console.log(targetData);
+   console.log(compData);
+})
+.catch((message)=> console.log(message));
